9b7222a1ddec6edd313fae2f7d0a2a35c89e93fc
Ccallata <1420015@utp.edu.pe>
2018-02-25 22:21:47 -0500

    implementing entity order

--- src/pe/edu/utp/javawebnovum/models/OrdersEntity.java
+++ src/pe/edu/utp/javawebnovum/models/OrdersEntity.java
@@ -1,5 +1,41 @@
 package pe.edu.utp.javawebnovum.models;
 
-public class OrdersEntity {
+import com.oracle.wls.shaded.org.apache.xpath.operations.Or;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrdersEntity extends BaseEntity{
+    private static String DEFAULT_SQL = "SELECT * FROM orders";
+    private List<Order> findByCriteria(String sql, OrdersEntity regionsEntity) {
+        List<Order> countries = new ArrayList<>();
+        if(getConnection() != null) {
+            try {
+                ResultSet resultSet = getConnection()
+                        .createStatement()
+                        .executeQuery(sql);
+                while(resultSet.next()) {
+                    Order country = new Order(
+                            //resultSet.getString("country_id"),
+                            //resultSet.getString("country_name"),
+                            //regionsEntity
+                                    //.findById(resultSet
+                                            //.getInt("order_id"))
+                    );
+                    countries.add(country);
+                }
+                return countries;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+
+    }
+
+    public List<Order> findAll(OrdersEntity regionsEntity) {
+        return findByCriteria(DEFAULT_SQL, regionsEntity);
+    }
 }
